f7807172f1ee0887cd04e1d24a0f49382bdcbb56
Riginana <rigina.rysbekova@gmail.com>
2021-04-14 22:18:36 +0200

    almost done

--- Guess_my_number/script.js
+++ Guess_my_number/script.js
@@ -2,16 +2,33 @@
 
 let secretNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
 
 let check = document.querySelector('.check').addEventListener('click', function(){
     let guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess);
 
+    // when there is no input
     if(!guess){
         document.querySelector('.message').textContent = 'No Number!'
+
+    // when player wins
     } else if(guess === secretNumber){
         document.querySelector('.message').textContent = 'Correct Number!';
+
+        document.querySelector('.number').textContent = secretNumber;
+
+        let body = document.querySelector('body');
+        let number = document.querySelector('.number');
+
+        body.style.background = 'green';
+        number.style.width = '300px';
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    // when guess is too high
     } else if(guess > secretNumber){
 
         if(score > 1){
@@ -23,6 +40,7 @@ let check = document.querySelector('.check').addEventListener('click', function(
             document.querySelector('.score').textContent = 0;
         }
 
+    // when guess is too low
     } else if(guess < secretNumber){
 
         if(score > 1){
@@ -35,3 +53,19 @@ let check = document.querySelector('.check').addEventListener('click', function(
         }
     }
 })
+
+
+let again = document.querySelector('.again');
+again.addEventListener('click', function(){
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    score = 20;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = '';
+    let body = document.querySelector('body');
+    let number = document.querySelector('.number');
+    body.style.background = '#222';
+    number.style.width = '15rem';
+
+});
